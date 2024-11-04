73271011614bfebfb24c80e827956d06c35eb7eb
Taisei <taisei.960420@gmail.com>
2024-11-04 15:32:59 +0900

    カテゴリ表示

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ CommonModule ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
+  public categoryList: string[] = [
+    'Fridge',
+    'Washer',
+    'Kitchen',
+    'Vacuum',
+    'Climate',
+    'TV',
+    'Other',
+  ];
 
+  public trackByIndex(index: number, item: any): number {
+    return index;
+  }
 }
